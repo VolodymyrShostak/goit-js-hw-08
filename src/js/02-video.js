aa18fb378367124f47dcd9ba201883d6405d83d6
VolodymyrShostak <110408009+VolodymyrShostak@users.noreply.github.com>
2022-10-20 22:13:56 +0300

    закінчення №2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,12 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
 const vimeoElement = document.querySelector('#vimeo-player');
 const player = new Player(vimeoElement);
-player.setCurrentTime(localStorage.getItem('videoplaayer-current-time'));
-player.on('timeupdate', event => {
-  localStorage.setItem('videoplaayer-current-time', event.seconds);
+const STORAGE_KEY = 'videoplaayer-current-time';
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
+const getTime = event => {
+  localStorage.setItem(STORAGE_KEY, event.seconds);
   console.log(localStorage);
-});
+};
+player.on('timeupdate', throttle(getTime, 1000));
