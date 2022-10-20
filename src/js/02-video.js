9e73737bc523be68d714919eebcf808e9c9d36e2
VolodymyrShostak <110408009+VolodymyrShostak@users.noreply.github.com>
2022-10-20 20:27:28 +0300

    завдання 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,8 @@
-
+import Player from '@vimeo/player';
+const vimeoElement = document.querySelector('#vimeo-player');
+const player = new Player(vimeoElement);
+player.setCurrentTime(localStorage.getItem('videoplaayer-current-time'));
+player.on('timeupdate', event => {
+  localStorage.setItem('videoplaayer-current-time', event.seconds);
+  console.log(localStorage);
+});
