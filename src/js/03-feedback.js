8c47f34fd6ac728ac8b6299dccc91f4a4c797a38
VolodymyrShostak <110408009+VolodymyrShostak@users.noreply.github.com>
2022-10-21 03:22:42 +0300

    завдання №3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,24 @@
+import throttle from 'lodash.throttle';
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
 
+let formData = {};
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(saveData, 500));
+
+if (localStorage.getItem(STORAGE_KEY)) {
+  formData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  for (let key in formData) {
+    form.elements[key].value = formData[key];
+  }
+}
+function saveData(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+function onFormSubmit(e) {
+  e.preventDefault();
+  console.log(formData);
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
