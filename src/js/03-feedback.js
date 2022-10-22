9a9cb9d25fba38c3138d142fda630989db1dddcb
VolodymyrShostak <110408009+VolodymyrShostak@users.noreply.github.com>
2022-10-22 18:59:09 +0300

    редагування коду задачі 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,23 +2,24 @@ import throttle from 'lodash.throttle';
 const STORAGE_KEY = 'feedback-form-state';
 const form = document.querySelector('.feedback-form');
 
+let formData = {};
 form.addEventListener('submit', onFormSubmit);
 form.addEventListener('input', throttle(saveData, 500));
 
-function saveData(e) {
-  const formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
-  formData[e.target.name] = e.target.value;
-  if (localStorage.getItem(STORAGE_KEY)) {
-    for (let key in formData) {
-      form.elements[key].value = formData[key];
-    }
+if (localStorage.getItem(STORAGE_KEY)) {
+  formData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  for (let key in formData) {
+    form.elements[key].value = formData[key];
   }
+}
+function saveData(e) {
+  formData[e.target.name] = e.target.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
-
 function onFormSubmit(e) {
   e.preventDefault();
-  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+  console.log(formData);
   e.currentTarget.reset();
+  formData = {};
   localStorage.removeItem(STORAGE_KEY);
 }
