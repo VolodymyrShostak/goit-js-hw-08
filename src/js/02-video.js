c34183b2d730cfc60755967acaa2d352171dd8cc
VolodymyrShostak <110408009+VolodymyrShostak@users.noreply.github.com>
2022-10-23 11:37:55 +0300

    версія 2 завдання 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,12 +4,10 @@ import throttle from 'lodash.throttle';
 const vimeoElement = document.querySelector('#vimeo-player');
 const player = new Player(vimeoElement);
 const STORAGE_KEY = 'videoplaayer-current-time';
-if (localStorage.getItem(STORAGE_KEY)) {
-  player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
-}
-
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
 const getTime = event => {
   localStorage.setItem(STORAGE_KEY, event.seconds);
-  console.log(localStorage);
+
+  console.log(localStorage || 0);
 };
 player.on('timeupdate', throttle(getTime, 1000));
