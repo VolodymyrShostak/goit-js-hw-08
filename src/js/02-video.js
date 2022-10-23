c800b43973555081ca383d360d4bb92692a9a643
VolodymyrShostak <110408009+VolodymyrShostak@users.noreply.github.com>
2022-10-23 12:05:47 +0300

    fjfjkj

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,10 +4,8 @@ import throttle from 'lodash.throttle';
 const vimeoElement = document.querySelector('#vimeo-player');
 const player = new Player(vimeoElement);
 const STORAGE_KEY = 'videoplaayer-current-time';
-player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY)).then();
 const getTime = event => {
   localStorage.setItem(STORAGE_KEY, event.seconds);
-
-  console.log(localStorage || 0);
 };
 player.on('timeupdate', throttle(getTime, 1000));
