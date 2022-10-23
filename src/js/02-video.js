3a857c43212c6963728dfd3c3ddd4a31c69eecc0
VolodymyrShostak <110408009+VolodymyrShostak@users.noreply.github.com>
2022-10-23 12:44:09 +0300

    аорпрпло

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,7 +7,9 @@ const STORAGE_KEY = 'videoplaayer-current-time';
 if (localStorage.getItem(STORAGE_KEY)) {
   player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
 }
+player.on('timeupdate', throttle(getTime, 1000));
+
 const getTime = event => {
   localStorage.setItem(STORAGE_KEY, event.seconds);
+  console.log(event.seconds);
 };
-player.on('timeupdate', throttle(getTime, 1000));
